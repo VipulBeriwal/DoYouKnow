6a0080774e9581ebb561856cd4b4b0c22c3e07f5
Vipul Beriwal <vipul.beriwal@ufl.edu>
2018-10-28 03:02:37 -0400

    Create PythonStringSlicing.py

---
+++ PythonStringSlicing.py
@@ -0,0 +1,42 @@
+'''
+Rules -
+
+RULE1-
+  if start point is not mentioned,
+    then default start is 0 if stride > 0 
+  else:
+    start point is last character of string that is -1.a nd start is -1
+
+RULE2-
+  Negative stride means that move backwards from that index/position by n steps if provided otherwise 1
+  
+RULE3-
+  If moving backward, the string ends, iterator will stop.
+  If moving forwards. the string ends, iterator will stop.
+  
+RULE4-
+  If moving backward, and end > start, it will not print it.
+  if moving forward, end <start, it will not print it.
+  
+'''
+
+ss = "vipul beriwal"
+
+print(ss[4]) 
+print(ss[-3]) 
+print(ss[6:11]) 
+print(ss[:5]) 
+print(ss[7:]) 
+print(ss[-4:-1]) 
+print(ss[6:11:1]) 
+print(ss[0:12:2]) 
+print(ss[0:12:4]) 
+print(ss[::4]) 
+print(ss[::-1])
+print(ss[::-2]) 
+print(ss[6:1:-3])
+print(ss[-4:-1:-1]) 
+print(ss[-4:-5:-1]) 
+print(ss[::1])
+print(ss[1:0:1])
+print(ss[0::-1])
