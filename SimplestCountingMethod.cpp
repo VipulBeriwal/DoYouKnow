cfe9d6d315d81fd982f57e4e8b561a587c47be97
Vipul Beriwal <vipul.beriwal@ufl.edu>
2018-10-28 03:45:08 -0400

    Update SimplestCountingMethod.cpp

--- SimplestCountingMethod.cpp
+++ SimplestCountingMethod.cpp
@@ -3,7 +3,7 @@ we have 256 numbers in acsii table. Now, which enconding we apply depends on us.
 For eg encoding could be  = > int(decimal form), hex, char etc. 
 If we have a char variable it can be treated as 1 byte integar whiule passing that in the index of array"
 Since, strings internally implement itself as a char we can apply this!
-"
+*/
 
 //Program counts the number of occurences 
 #include <iostream>
